d3d3003c754a69dfedca8b1e038803b89e33e68b
alumno <alumno@iMac-V108-06.local>
2024-09-09 10:12:46 -0600

    [CAMBIA] de ubicacion el relleno del diccionario a la estructura IUBotonCalculadora

--- Calculadora Lola/ViewController.swift
+++ Calculadora Lola/ViewController.swift
@@ -6,50 +6,38 @@
 //
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
-    //@IBOutlet weak var boton_para_interactuar: UIButton!
-    
     @IBOutlet weak var texto_a_cambiar: UILabel!
-    @IBOutlet weak var boton operacion: UILabel!
+    @IBOutlet weak var boton_operacion: UIButton!
+  //  @IBOutlet weak var BOTONES: UIStackView!
     
-
-    var botones_interfaz: Dictionary<String, IUBotonCalculadora>
+    var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
     
-  // V2:  var botones_interfaz: Array<IUBotonCalculadora> = []
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         inicializar_calculadora()
     }
-    
-    
+
+    /// <#Description#>
+    /// - Parameter sender: <#sender description#>
     @IBAction func que_hacer_puchar_boton(_ sender: UIButton) {
-        //  texto_a_cambiar.text = sender.titleLabel!.text ?? "NOsupe"
-        /*   print(sender.restorationIdentifier)
-         texto_a_cambiar.text = texto_a_cambiar.text + sender.restorationIdentifier
-         */
-        // boton_para_interactuar.titleLbel
-     /*V2:   for boton in botones_interfaz{
-            if(boton.restorationID == sender.restorationIdentifier){
-                print(boton.numero)
-            }*/
-        let text_a_añadir =
-        botones_interfaz[sender.restorationIdentifier}
-        }
+        // TODO: Arreglar glitch del text quitando el optional
+        let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
+        texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
     }
-    
+
     func inicializar_calculadora() -> Void{
         crear_arreglo_botones()
     }
     
-    func crear_arreglo_botones() -> Void{
-        for numero in 0...9{
-            botones_interfaz["boton_\(numero)"] = IUBotonCalculadora(
-                    "boton_\(numero)",
-                    numero: Character("\(numero)"),
-                    operacion: ""
-        }
+    func crear_arreglo_botones() -> Void {
+       
+        botones_interfaz = IUBotonCalculadora.crear_arreglo_botones()
+        
+        
+    
     }
+ 
 }
-
